baa5624b62e2e858420958a6d92aef4b53776e87
Mohit Meshram <mohitmeshram44@gmail.com>
2025-08-08 14:41:16 +0530

    Day-4

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terrabucketname"
     key    = "Day-4/terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true #s3 supports this features but terraform version 1 > 1.10, latest version >= 1.10 
   }
 }
