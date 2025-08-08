8d10b4def4fcd5a02373cf261e9fa2e5fe48c494
Mohit Meshram <mohitmeshram44@gmail.com>
2025-08-08 16:33:41 +0530

    Day-4

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -4,5 +4,7 @@ terraform {
     key    = "Day-4/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true #s3 supports this features but terraform version 1 > 1.10, latest version >= 1.10 
+  dynamodb_table = "test"
+  encrypt =true
   }
 }
